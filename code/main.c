b06e9b7ee9272526d0b812c30f5b9342fa822bd0
Paulo Henrique Pimentel <paulohenrique.pimentel@gmail.com>
2020-06-17 14:08:07 -0300

    Update main.c

--- code/main.c
+++ code/main.c
@@ -4,17 +4,18 @@
 
 int main()
 {
-
-
     int opcao = 1;
     int carregou = 0;
     char nomeArquivo[20];
+    
     while (opcao == 1 || opcao == 2){
+        
         printf("PROGRAMA Labirinto: Opcoes do programa:\n");
         printf("1) Carregar novo arquivo de dados.\n");
         printf("2) Processar e exibir resposta.\n");
         printf("3 ou qualquer outro caracter) Sair do programa.\n");
         printf("Digite um numero:\n");
+        
         scanf(" %d", &opcao);
         if (opcao == 1){
             carregou = 1;
@@ -23,11 +24,9 @@ int main()
         } else if (opcao == 2 && carregou == 1){
             FILE* arquivo = fopen(nomeArquivo, "r");
             iniciaJogo(arquivo);
-
         } else{
             printf("Por favor carregue antes um arquivo de dados! \n");
         }
     }
 return 0;
 }
-
